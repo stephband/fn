80be70005e9862ecb9a5804b172bc8d7c8c01253
stephband <stephband@googlemail.com>
2016-07-01 17:36:38 +0200

    Adds second, year, month etc to time.floor(string)

--- js/time.js
+++ js/time.js
@@ -2,7 +2,7 @@
 	var Fn = window.Fn;
 
 	// Be generous with the input we accept.
-	var rdate     = /^(-)?(\d+)-(\d+)-(\d+)$/;
+	var rdate     = /^(-)?(\d{4})(?:-(\d+))?(?:-(\d+))?$/;
 	var rtime     = /^(-)?(\d+):(\d+)(?::(\d+(?:\.\d+)?))?$/;
 	var rdatetime = /^(-)?(\d+)-(0[0-9]|1[12])-([0-2][0-9]|3[01])T([01][0-9]|2[0-3]):([0-5][0-9])(?::([0-5][0-9](?:\.\d+)?))?/;
 
@@ -14,14 +14,11 @@
 		if (!tokens) { throw new Error('Time: "' + time + '" does not parse as time.'); }
 
 		var sign = tokens[1] ? -1 : 1 ;
-		var adds = tokens.slice(2).map(parseFloat);
 
-		if (adds[2]) {
-			date.setUTCMilliseconds(date.getUTCMilliseconds() + sign * adds[2] * 1000);
-		}
+		if (Fn.isDefined(tokens[4])) { date.setUTCMilliseconds(date.getUTCMilliseconds() + sign * parseFloat(tokens[4]) * 1000); }
+		if (Fn.isDefined(tokens[3])) { date.setUTCMinutes(date.getUTCMinutes() + sign * parseInt(tokens[3], 10)); }
+		if (Fn.isDefined(tokens[2])) { date.setUTCHours(date.getUTCHours() + sign * parseInt(tokens[2], 10)); }
 
-		date.setUTCMinutes(date.getUTCMinutes() + sign * adds[1]);
-		date.setUTCHours(date.getUTCHours() + sign * adds[0]);
 		return date;
 	}
 
@@ -30,17 +27,19 @@
 
 		if (!tokens) { throw new Error('Time: "' + time + '" does not parse as date.'); }
 
-		var sign   = tokens[1] ? -1 : 1 ;
-		var adds   = tokens.slice(2).map(parseFloat);
+		var sign = tokens[1] ? -1 : 1 ;
 
-		date.setUTCDate(date.getUTCDate() + sign * adds[2]);
-		date.setUTCMonth(date.getUTCMonth() + sign * adds[1]);
-		date.setUTCFullYear(date.getUTCFullYear() + sign * adds[0]);
+		if (Fn.isDefined(tokens[4])) { date.setUTCDate(date.getUTCDate() + sign * parseInt(tokens[4], 10)); }
+		if (Fn.isDefined(tokens[3])) { date.setUTCMonth(date.getUTCMonth() + sign * parseInt(tokens[3], 10)); }
+		if (Fn.isDefined(tokens[2])) { date.setUTCFullYear(date.getUTCFullYear() + sign * parseInt(tokens[2], 10)); }
+
 		return date;
 	}
 
 	function Time(time) {
-		if (!Time.prototype.isPrototypeOf(this)) { return new Time(time); }
+		if (!Time.prototype.isPrototypeOf(this)) {
+			return new Time(time);
+		}
 
 		var date = time === undefined ? new Date(0) :
 			time instanceof Date ? time :
@@ -56,7 +55,6 @@
 		},
 
 		add: function(time) {
-			var tokens = rdate.exec(time);
 			var date  = new Date(this.toDate());
 
 			return new Time(rdate.test(time) ?
@@ -71,11 +69,32 @@
 				var day = grain;
 			}
 
-			if (typeof grain === 'string' && days[grain]) {
-				var day = days[grain];
+			var day = days[grain];
+			var date = new Date(this.toDate());
+
+			if (!Fn.isDefined(day)) {
+				date.setUTCMilliseconds(0);
+				if (grain === 'second') { return new Time(date); }
+
+				date.setUTCSeconds(0);
+				if (grain === 'minute') { return new Time(date); }
+
+				date.setUTCMinutes(0);
+				if (grain === 'hour') { return new Time(date); }
+
+				date.setUTCHours(0);
+				if (grain === 'day') { return new Time(date); }
+
+				date.setUTCDate(1);
+				if (grain === 'month') { return new Time(date); }
+
+				date.setUTCMonth(0);
+				if (grain === 'year') { return new Time(date); }
+
+				date.setUTCFullYear(0);
+				if (grain === 'century') { return new Time(date); }
 			}
 
-			var date = this.toDate();
 			var currentDay = date.getUTCDay();
 
 			// If we are on the specified day, return this date
@@ -88,6 +107,10 @@
 			return this.add('-0000-00-0' + diff);
 		}
 	});
+
+	// .toString() enables comparisons of the genre time1 > time2,
+	// but not equality checking, which does not coerce to string.
+	Time.prototype.toString = Time.prototype.toJSON;
 
 	Object.defineProperties(Time.prototype, {
 		date: {
