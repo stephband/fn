0e7b902c4d383138d43b8679a7d148eb15f9c51d
stephband <stephband@googlemail.com>
2016-07-19 04:25:04 +0200

    isDefined checks for NaN

--- js/fn.js
+++ js/fn.js
@@ -367,7 +367,7 @@
 		getPath: curry(function get(path, object) {
 			return object.get ? object.get(path) :
 				typeof path === 'number' ? object[path] :
-				path === '' ? object :
+				path === '' || path === '.' ? object :
 				objFrom(object, splitPath(path)) ;
 		}),
 
@@ -492,7 +492,7 @@
 		isDefined: function isDefined(value) {
 			// !!value is a fast out for non-zero numbers, non-empty strings
 			// and other objects, the rest checks for 0, '', etc.
-			return !!value || (value !== undefined && value !== null);
+			return !!value || (value !== undefined && value !== null && !Number.isNaN(value));
 		},
 
 		toType: function typeOf(object) {
