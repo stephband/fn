549f8c43db9984f68b8ea3e128e353be50891d41
stephband <stephband@googlemail.com>
2017-03-21 08:22:40 +0100

    Faster toArray()

--- js/fn.js
+++ js/fn.js
@@ -278,9 +278,24 @@
 	}
 
 	function toArray(object) {
-		return object.toArray ? object.toArray() :
-			typeof object.length === "number" ? A.slice.apply(object) :
-			[] ;
+		if (object.toArray) { return object.toArray(); }
+		
+		// Speed test for array conversion:
+		// https://jsperf.com/nodelist-to-array/27
+
+		var array = [];
+		var l = object.length;
+		var i;
+
+		if (typeof object.length !== 'number') { return array; }
+
+		array.length = l;
+
+		for (i = 0; i < l; i++) {
+			array[i] = object[i];
+		}
+
+		return array;
 	}
 
 	function toInt(object) {
