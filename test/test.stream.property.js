2ae4f6ff2369c61fca2708d66b9c469711633f77
stephband <stephband@googlemail.com>
2017-04-24 15:16:37 +0200

    Adds test again

--- test/test.stream.property.js
+++ test/test.stream.property.js
@@ -16,4 +16,20 @@ group('Stream.Property(name, object)', function() {
 
 		equals(2, i, 'Test count is wrong');
 	});
+
+	test('.stop()', function() {
+		var i = 0;
+		var o = { a: 0 };
+		var stream = Property('a', o).each(function(a) {
+			equals(++i, a);
+		});
+
+		o.a = 1;
+
+		stream.stop();
+
+		o.a = 2;
+
+		equals(1, i, 'Test count is wrong');
+	});
 });
