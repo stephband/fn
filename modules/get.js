a778daccdd904296e97713c9c16cee1b1f16bc88
stephband <stephband@googlemail.com>
2018-08-15 00:10:04 +0200

    get() will return null values rather than coercing to undefined

--- modules/get.js
+++ modules/get.js
@@ -2,9 +2,9 @@
 export default function get(key, object) {
     // Todo? Support WeakMaps and Maps and other map-like objects with a
     // get method - but not by detecting the get method
-    const value = object[key];
+    return object[key];
 
     // Why are we protecting against null again? To innoculate ourselves
     // against DOM nodes?
-    return value === null ? undefined : value ;
+    //return value === null ? undefined : value ;
 }
