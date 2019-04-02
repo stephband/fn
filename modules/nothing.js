4efc4126960fa505519e0abfa2889c532517a8ba
stephband <stephband@googlemail.com>
2019-04-02 15:38:01 +0200

    nothing had no length, oddly

--- modules/nothing.js
+++ modules/nothing.js
@@ -1,7 +1,8 @@
 import noop from './noop.js';
 
-export default Object.freeze(Object.defineProperties({}, {
-   shift: { value: noop },
-   push:  { value: noop },
-   stop:  { value: noop }
-}));
+export default Object.freeze({
+    shift: noop,
+    push:  noop,
+    stop:  noop,
+    length: 0
+});
