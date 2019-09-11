c479949067343ca07ac85787448b46b4d20444b4
stephband <stephband@googlemail.com>
2019-09-11 18:18:20 +0200

    Make nothing iterable

--- modules/nothing.js
+++ modules/nothing.js
@@ -1,8 +1,19 @@
 import noop from './noop.js';
 
+const done     = { done: true };
+const iterator = { next: () => done };
+
 export default Object.freeze({
+    // Standard array methods
     shift: noop,
     push:  noop,
+
+    // Stream methods
     stop:  noop,
-    length: 0
+
+    // Make it look like an empty array
+    length: 0,
+
+    // Make it an iterable with nothing in it
+    [Symbol.iterator]: () => iterator
 });
