7fd45fe5606c5b3b13c8ccba1a9e98046dfb5e46
stephband <stephband@googlemail.com>
2019-12-09 18:25:27 +0100

    Build

--- modules/stream.js
+++ modules/stream.js
@@ -577,9 +577,9 @@ Stream.fromPromise = function(promise) {
 
 /*
 Stream.fromProperty(name, object)
-Returns a stream of mutations made to the `name` property of `object`
-(assuming those mutations are made to the [Observer](#observer) proxy of object - see
-Observer).
+Returns a stream of mutations made to the `name` property of `object`,
+assuming those mutations are made to the Observer proxy of object - see
+[Observer](#observer).
 */
 
 
