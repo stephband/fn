2ee940f6dd3d60a6b2cf72b9274861c10bab06fd
stephband <stephband@googlemail.com>
2020-12-21 16:40:17 +0100

    equals

--- modules/equals.js
+++ modules/equals.js
@@ -22,20 +22,20 @@ export function equals(a, b) {
 
     // Compare their enumerable keys
     const akeys = Object.keys(a);
-    let n = akeys.length;
 
+    let n = akeys.length;
     while (n--) {
         // Has the property been set to undefined on a?
         if (a[akeys[n]] === undefined) {
             // We don't want to test if it is an own property of b, as
             // undefined represents an absence of value
-            if (b[akeys[n]] === undefined) {
-                return true;
+            if (b[akeys[n]] !== undefined) {
+                return false;
             }
         }
         else {
-            //
-            if (b.hasOwnProperty(akeys[n]) && !equals(a[akeys[n]], b[akeys[n]])) {
+            // console.log(equals(a[akeys[n]], b[akeys[n]]), akeys[n], a[akeys[n]], b[akeys[n]]);
+            if (!b.hasOwnProperty(akeys[n]) || !equals(a[akeys[n]], b[akeys[n]])) {
                 return false;
             }
         }
