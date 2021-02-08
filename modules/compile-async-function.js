a7d5797243ad674a7e734a1b33dc005a17aa307f
stephband <stephband@googlemail.com>
2021-02-08 08:37:41 +0100

    Fn

--- modules/compile-async-function.js
+++ modules/compile-async-function.js
@@ -2,13 +2,14 @@
 /** 
 compileAsyncFunction(scope, paramString, code, context)
 
-Compiles a new async function from `code` that runs in `scope` with the arguments 
-listed in `paramString`. If a `context` is passed in, you get an async arrow 
-function, otherwise an async function (leaving you the possibility to set the 
-context at runtime with `fn.apply(context, params)`).
+Compiles a new async function from `code` that runs in `scope` with the 
+arguments listed in `paramString`. If a `context` is passed in, you get an 
+async arrow function, otherwise an async function (leaving you the possibility 
+to set the context at runtime by invoking it as a method, 
+or via `fn.apply(context, params)`).
 **/
 
-export default function compileAsyncFunction(scope, paramString, code, context) {
+export default function compileAsyncFunction(scope, paramString, code, context, name) {
     const keys   = Object.keys(scope);
     const values = Object.values(scope);
     return context ?
@@ -16,6 +17,6 @@ export default function compileAsyncFunction(scope, paramString, code, context) 
         new Function(...keys, 'return async (' + paramString + ') => {' + (code || '') + '}')
         .apply(context, values) :
         // This function can be called with a context fn.call(context, data)
-        new Function(...keys, 'return async function(' + paramString + '){' + (code || '') + '}')
+        new Function(...keys, 'return async function' + (name ? ' ' + name : '') + '(' + paramString + '){' + (code || '') + '}')
         .apply(null, values) ;
 }
