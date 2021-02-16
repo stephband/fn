8c6addd23a9db070831a51ddb0ab58826684ff3b
stephband <stephband@googlemail.com>
2021-02-16 05:53:02 +0100

    Types

--- modules/types.js
+++ modules/types.js
@@ -1,126 +1,113 @@
 import noop from './noop.js';
 
-const DEBUG = false;//window.DEBUG === undefined || window.DEBUG;
+const DEBUG = window.DEBUG === true;
 
 const defs = {
-    // Primitive types
+    'any': noop,
+
+    'array': (value) =>
+        Array.isArray(value),
+    
+    'array-like': (value) =>
+        typeof value.length === 'number',
 
     'boolean': (value) =>
         typeof value === 'boolean',
 
-    'function': (value) =>
-        typeof value === 'function',
-
-    'number': (value) =>
-        typeof value === 'number',
-
-    'object': (value) =>
-        typeof value === 'object',
-
-    'symbol': (value) =>
-        typeof value === 'symbol',
-
-    // Functional types
-    // Some of these are 'borrowed' from SancturyJS
-    // https://github.com/sanctuary-js/sanctuary-def/tree/v0.19.0
-
-    'Any': noop,
-
-    'Array': (value) =>
-        Array.isArray(value),
-
-    'ArrayLike': (value) =>
-        typeof value.length === 'number',
-
-    'Boolean': (value) =>
-        typeof value === 'boolean',
-
-    'Date': (value) =>
+    'date': (value) =>
         value instanceof Date
         && !Number.isNaN(value.getTime()),
 
-    'Error': (value) =>
+    'error': (value) =>
         value instanceof Error,
 
-    'Integer': (value) =>
+    'function': (value) =>
+        typeof value === 'function',
+
+    'int': (value) =>
         Number.isInteger(value)
         && Number.MIN_SAFE_INTEGER <= value
         && Number.MAX_SAFE_INTEGER >= value,
 
-    'NegativeInteger': (value) =>
+    'int<0': (value) =>
         Number.isInteger(value)
         && Number.MIN_SAFE_INTEGER <= value
         && Number.MAX_SAFE_INTEGER >= value
         && value < 0,
-
-    'NonPositiveInteger': (value) =>
+    
+    'int<=0': (value) =>
         Number.isInteger(value)
         && Number.MIN_SAFE_INTEGER <= value
         && Number.MAX_SAFE_INTEGER >= value
         && value <= 0,
 
-    'PositiveInteger': (value) =>
+    'int>0': (value) =>
         Number.isInteger(value)
         && Number.MIN_SAFE_INTEGER <= value
         && Number.MAX_SAFE_INTEGER >= value
         && value > 0,
 
-    'NonNegativeInteger': (value) =>
+    'int>=0': (value) =>
         Number.isInteger(value)
         && Number.MIN_SAFE_INTEGER <= value
         && Number.MAX_SAFE_INTEGER >= value
         && value >= 0,
 
-    'Number': (value) =>
-        typeof value === 'number'
-        && !Number.isNaN(value),
+    'number': (value) =>
+        typeof value === 'number' && !Number.isNaN(value),
 
-    'NegativeNumber': (value) =>
+    'number<0': (value) =>
         typeof value === 'number'
         && value < 0,
 
-    'NonPositiveNumber': (value) =>
+    'number<=0': (value) =>
         typeof value === 'number'
         && value <= 0,
 
-    'PositiveNumber': (value) =>
+    'number>0': (value) =>
         typeof value === 'number'
         && value > 0,
 
-    'NonNegativeNumber': (value) =>
+    'number>=0': (value) =>
         typeof value === 'number'
         && value >= 0,
 
-    'Null': (value) =>
-        value === null,
-
-    'Object': (value) =>
+    'object': (value) =>
         !!value
         && typeof value === 'object',
 
-    'RegExp': (value) =>
-        value instanceof RegExp
+    'regexp': (value) =>
+        value instanceof RegExp,
+
+    'symbol': (value) =>
+        typeof value === 'symbol',
+
+    'null': (value) =>
+        value === null
 };
 
-export const checkType = DEBUG ? function checkType(type, value, file, line, message) {
+export const checkType = DEBUG ? function checkType(type, value, message) {
     if (!defs[type]) {
-        throw new RangeError('Type "' + type + '" not recognised');
+        if (/^[A-Z]/.test(type)) {
+            if (value.constructor.name === type) { return; }
+            throw new Error(message || 'value not of type "' + type + '": ' + value);
+        }
+
+        throw new Error('Type "' + type + '" not recognised');
     }
 
     if (!defs[type](value)) {
-        throw new Error(message || 'value not of type "' + type + '": ' + value, file, line);
+        throw new Error(message || 'value not of type "' + type + '": ' + value);
     }
 } : noop ;
 
-export const checkTypes = DEBUG ? function checkTypes(types, args, file, line) {
-    var n = types.length;
-
-    while (n--) {
-        checkType(types[n], args[n], file, line, 'argument ' + n + ' not of type "' + types[n] + '": ' + args[n]);
-    }
+export const validate = DEBUG ? function checkTypes(types, object) {
+    Object.entries(types).forEach(([key, type]) =>
+        checkType(type, object[key], key + ' not of type "' + type + '" (' + object[key] + ')')
+    );
 } : noop ;
 
-export function def(notation, fn, file, line) {
+export const def = DEBUG ? function def(notation, fn, file, line) {
     // notation is of the form:
     // 'Type, Type -> Type'
     // Be generous with what we accept as output marker '->' or '=>'
@@ -128,10 +115,19 @@ export function def(notation, fn, file, line) {
     var types = parts[0].split(/\s*,\s*/);
     var returnType = parts[1];
 
-    return DEBUG ? function() {
-        checkTypes(types, arguments, file, line);
+    return function() {
+        validate(types, arguments);
         const output = fn.apply(this, arguments);
-        checkType(returnType, output, file, line, 'return value not of type "' + returnType + '": ' + output);
+        checkType(returnType, output, 'return value not of type "' + returnType + '": ' + output);
         return output;
-    } : fn ;
+    };
+} : function def(notation, fn) {
+    return fn;
+} ;
+
+
+/* Deprecated */
+
+export function checkTypes() {
+    throw new Error('modules/types.js checkTypes() is now validate()');
 }
