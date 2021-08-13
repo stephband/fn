9df0827a8c0cf9fcadd653fe9019e5d12176e68b
stephband <stephband@googlemail.com>
2021-08-13 20:28:48 +0200

    Removes archive/, rests bad pool

--- archive/range-log-inv.js
+++
@@ -1,5 +0,0 @@
-import normalise from './normalise.js';
-
-export default function rangeLogInv(min, max, n) {
-    return min * Math.pow(max / min, normalise(min, max, n));
-};
--- archive/range-log.js
+++
@@ -1,5 +0,0 @@
-import denormalise from './denormalise.js';
-
-export default function rangeLog(min, max, n) {
-    return denormalise(min, max, Math.log(n / min) / Math.log(max / min));
-};
