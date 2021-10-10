0d515fb4c45ca594e4b48cc3b7968c92f07c2f63
stephband <stephband@googlemail.com>
2021-10-10 06:54:16 +0200

    Warning for cache()

--- modules/cache.js
+++ modules/cache.js
@@ -10,11 +10,11 @@ export default function cache(fn) {
 
     return function cache(object) {
         if (window.DEBUG && object === undefined) {
-            throw new Error('Fn: cache() called with undefined.');
+            console.warn('Fn: cache() called with undefined.');
         }
 
         if (window.DEBUG && arguments.length > 1) {
-            throw new Error('Fn: cache() called with ' + arguments.length + ' arguments. Accepts exactly 1.');
+            console.warn('Fn: cache() called with ' + arguments.length + ' arguments. Accepts exactly 1.');
         }
 
         if (map.has(object)) {
