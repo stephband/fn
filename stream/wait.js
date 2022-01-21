6a49f443664e4f56d1308942d283c4f5ebfdf5e0
Stephen <stephband@cruncher.ch>
2022-01-21 19:06:51 +0100

    Push

--- stream/wait.js
+++ stream/wait.js
@@ -1,16 +1,18 @@
 
 import Stream from './stream.js';
 
-function push(source, value) {
-    source.push(value);
+function push(source, p, value) {
+    p.call(source, value);
 }
 
 export default function Wait(time) {
     let timer;
-    return new Stream((source) => ({
-        push: function(value) {
+    return new Stream((source) => {
+        const p = source.push;
+        // The way to make a stream pushable is to override push on source
+        source.push = function(value) {
             clearTimeout(timer);
-            timer = setTimeout(push, time * 1000, source, value);
-        }
-    }));
+            timer = setTimeout(push, time * 1000, source, p, value);
+        };
+    });
 }
