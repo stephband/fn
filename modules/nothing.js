052a21ce340e47401339be7a39823153e22652f2
Stephen <stephband@cruncher.ch>
2022-10-20 18:32:01 +0200

    Adds methods to nothing

--- modules/nothing.js
+++ modules/nothing.js
@@ -1,32 +1,44 @@
 
+
+import arg  from './arg.js';
 import id   from './id.js';
 import noop from './noop.js';
+import self from './self.js';
 
+const assign = Object.assign;
+const create = Object.create;
 const freeze = Object.freeze;
 
-function self() {
-    return this;
-}
+function returnTrue() { return true; }
+function negative()   { return -1; }
 
-export default freeze({
+export default freeze(assign(create({
     // Array methods
-    shift:    noop,
-    push:     noop,
-    forEach:  noop,
-    join:     function() { return ''; },
-    map:      self,
-    filter:   self,
-    includes: function() { return false; },
-    reduce:   function(fn, accumulator) { return accumulator; },
-    length:   0,
+    shift:     noop,
+    push:      noop,
+    forEach:   noop,
+    join:      function() { return ''; },
+    every:     returnTrue,
+    filter:    self,
+    find:      noop,
+    findIndex: negative,
+    flat:      self,
+    flatMap:   self,
+    includes:  function() { return false; },
+    indexOf:   negative,
+    map:       self,
+    reduce:    arg(1),
+    sort:      self,
 
    // Stream methods
-    each:     noop,
-    pipe:     id,
-    start:    noop,
-    stop:     noop,
-    done:     noop,
+    each:      self,
+    pipe:      id,
+    start:     self,
+    stop:      self,
+    done:      self,
 
     // Primitive coercion
-    valueOf: function() { return null; }
-});
+    valueOf:   function() { return null; }
+}), {
+    length:    0,
+}));
