bd8f21df7fccf2432987e61b5d7d00284ae7608c
Stephen <stephband@cruncher.ch>
2022-12-09 08:08:46 +0100

    Test details

--- modules/test.js
+++ modules/test.js
@@ -1,14 +1,15 @@
 
 import { equals } from './equals.js';
 
-const tests = [];
-const stops = [];
+const tests  = [];
+const stops  = [];
+const totals = { pass: 0, fail: 0 };
 let running = false;
 
 function assert(expected, value, name, message) {
 	if (!equals(value, expected)) {
-		var string = '✘ ' + name + ' failed\n  '
-			+ 'Expected: ' + (typeof expected === 'object' ? JSON.stringify(expected) : expected) + ', '
+		var string = '✘ ' + name + '\n  '
+			+ 'expected: ' + (typeof expected === 'object' ? JSON.stringify(expected) : expected) + ', '
 			+ 'received: ' + (typeof value    === 'object' ? JSON.stringify(value)    : value)
 			+ ( message ? '\n  ' + message : '') ;
 
@@ -20,9 +21,11 @@ function assert(expected, value, name, message) {
 }
 
 function expectDone(expected, value, name) {
-	var string = '✘ ' + name + ' failed\n  '
+	var string = '✘ ' + name + '\n  '
 		+ 'Assertion after test stopped with done()' ;
 
+	--totals.pass;
+	++totals.fail;
 	console.log('%c' + string, 'color: #ee8833; font-weight: 300;');
 }
 
@@ -33,8 +36,8 @@ function run(name, expected, fn, next) {
 
 	let expect = (value, message) => {
 		if (!expected.length) {
-			var string = '✘ ' + name + ' failed\n  '
-				+ 'Expected ' + n + ' assertions, '
+			var string = '✘ ' + name + '\n  '
+				+ 'expected ' + n + ' assertions, '
 				+ 'received ' + (n + (++m)) + ': '
 				+ value ;
 
@@ -42,14 +45,14 @@ function run(name, expected, fn, next) {
 			pass = false;
 		}
 		else {
-			pass = pass && assert(expected.shift(), value, name, message);
+			pass = pass && assert(expected.shift(), value, name + ' (assertion ' + (n - expected.length) + ')', message);
 		}
 	};
 
 	fn(expect, function done() {
 		if (pass && expected.length) {
-			var string = '✘ ' + name + ' failed\n  '
-				+ 'Expected ' + n + ' assertions, '
+			var string = '✘ ' + name + '\n  '
+				+ 'expected ' + n + ' assertions, '
 				+ 'received ' + (n - expected.length) ;
 
 			console.trace('%c' + string, 'color: #ee8833; font-weight: 300;');
@@ -57,7 +60,11 @@ function run(name, expected, fn, next) {
 		}
 
 		if (pass) {
+			++totals.pass;
 			console.log('%c✔%c %s', 'color: #b4d094;', 'color: #6f9940; font-weight: 300;', name);
+		}
+		else {
+			++totals.fail;
 		}
 
 		expect = expectDone;
@@ -70,7 +77,7 @@ function next() {
 
 	if (!args) {
 		running = false;
-		stops.forEach((fn) => fn());
+		stops.forEach((fn) => fn(totals));
 		return;
 	}
 
@@ -78,18 +85,36 @@ function next() {
 	run(args.name, args.expected, args.fn, next);
 }
 
+/**
+test(name, expected, fn)
+A small test function designed to run in the browser.
+
+```js
+test('Example test', [1], (expect, done) => {
+	// Test a value against expected values
+	expect(1);
+	// Signal that test is done
+	done();
+});
+```
+**/
+
 export default function test(name, expected, fn) {
 	tests.push({ name, expected, fn });
-
 	if (!running) { next(); }
 }
 
+/**
+done(fn)
+Register a callback function that is fired when all tests are complete.
+
+```js
+done((totals) => console.log('Pass', totals.pass, 'Fail', totals.fail);
+```
+**/
+
 export function done(fn) {
 	// Dodgy dodgy logic
-	if (!running) {
-		fn();
-	}
-	else {
-		stops.push(fn);
-	}
+	if (!running) { fn(totals); }
+	else { stops.push(fn); }
 }
