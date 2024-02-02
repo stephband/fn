70f1ed759a5b814ce7e97c81374964a088a2f6e6
Stephen <stephband@cruncher.ch>
2024-02-02 16:32:02 +0100

    Mix

--- modules/mix.js
+++ modules/mix.js
@@ -3,11 +3,11 @@
 Defines properties (and property descriptors) of mixin on object.
 **/
 
-export default function mix(a, b) {
-    const descriptors = Object.getOwnPropertyDescriptors(b);
+export default function mix(target, ...objects) {
+    const descriptors = objects.map(Object.getOwnPropertyDescriptors);
+    const descriptor  = Object.assign({}, ...descriptors);
 
     // Do not overwrite constructor property, no no no
-    delete descriptors.constructor;
-
-    return Object.defineProperties(a, descriptors);
+    delete descriptor.constructor;
+    return Object.defineProperties(target, descriptor);
 }
