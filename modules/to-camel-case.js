cde5ab4a97d882dc64d89796ab5d0515e5fdd885
Stephen <stephband@cruncher.ch>
2024-07-25 17:14:03 +0200

    Camel

--- modules/to-camel-case.js
+++ modules/to-camel-case.js
@@ -5,8 +5,8 @@ Capitalises any Letter following a `'-'` and removes the dash.
 **/
 
 export default function toCamelCase(string) {
-    // Be gracious in what we accept as input
-    return string.replace(/-(\w)?/g, function($0, letter) {
-        return letter ? letter.toUpperCase() : '';
+    // Be a little gracious in what we accept as input
+    return string.replace(/-+(\w)/g, function($0, letter) {
+        return letter.toUpperCase();
     });
 }
