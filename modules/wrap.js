51b3e4c9d67163644d6fe5cc364f21d3dbcaa587
Stephen <stephband@cruncher.ch>
2025-03-31 18:47:50 +0200

    Remove curry(wrap)

--- modules/wrap.js
+++ modules/wrap.js
@@ -2,11 +2,8 @@
 wrap(min, max, n)
 **/
 
-import curry from './curry.js';
 import mod   from './mod.js';
 
-export function wrap(min, max, n) {
+export default function wrap(min, max, n) {
     return min + mod(max - min, n - min);
 }
-
-export default curry(wrap);
