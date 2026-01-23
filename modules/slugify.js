e22455940cb6a7c860c7b66e6eaa00cf1ee26ab9
Stephen <stephband@cruncher.ch>
2026-01-23 13:56:37 +0000

    Better slugify()

--- modules/slugify.js
+++ modules/slugify.js
@@ -4,21 +4,25 @@ slugify(string)
 Replaces any series of non-word characters with a `'-'` and lowercases the rest.
 
 ```js
-    slugify('Party on #mydudes!') // 'party-on-mydudes'
+slugify('Party on #mydudes!') // 'party-on-mydudes'
 ```
 **/
 
 export default function slugify(string) {
     // Accept a number or string
-    string = typeof string === 'number' ?
-        string + '' : 
-        string.trim() ;
-
-    if (typeof string !== 'string') { return; }
+    if (typeof string === 'number') string = string + '';
+    if (typeof string !== 'string') return;
 
     return string
-    .toLowerCase()
-    .replace(/^[\W_]+/, '')
-    .replace(/[\W_]+$/, '')
-    .replace(/[\W_]+/g, '-');
+        .trim()
+        .toLowerCase()
+        // Normalize unicode (NFKC)
+        .normalize('NFKC')
+        // Remove characters that are genuinely problematic in URLs
+        .replace(/[\/\?#\[\]@!$&'()*+,;=\\]/g, '')
+        // Replace whitespace and remaining punctuation with hyphens
+        .replace(/[\s\._]+/g, '-')
+        // Strip hyphens from start/end
+        .replace(/^-+/, '')
+        .replace(/-+$/, '');
 }
